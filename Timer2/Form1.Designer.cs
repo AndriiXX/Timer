5e8f10e29c9b1820cc0c764dce32512fd892112f
admin <admin@DESKTOP-PNVIAD5>
2024-01-11 23:08:49 +0200

    Add project files.

---
+++ Timer2/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Timer2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            numericUpDown1 = new NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(261, 276);
+            button1.Name = "button1";
+            button1.Size = new Size(90, 23);
+            button1.TabIndex = 0;
+            button1.Text = "START!";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(468, 276);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 1;
+            button2.Text = "STOP!";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(380, 201);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(51, 23);
+            numericUpDown1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(numericUpDown1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private NumericUpDown numericUpDown1;
+    }
+}
---
+++ Timer2/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Timer2
+{
+    public partial class Form1 : Form
+    {
+        private System.Windows.Forms.Timer vTimer = new System.Windows.Forms.Timer();
+        private ErrorProvider errorProvider1 = new ErrorProvider();
+
+        public Form1()
+        {
+            InitializeComponent();
+            button2.Enabled = false;
+
+
+            vTimer.Tick += new EventHandler(ShowTimer);
+        }
+
+        private void ShowTimer(object vObject, EventArgs e)
+        {
+
+            int remainingSeconds = Decimal.ToInt32(numericUpDown1.Value);
+            remainingSeconds--;
+
+
+            numericUpDown1.Value = Math.Max(remainingSeconds, 0);
+
+            if (remainingSeconds <= 0)
+            {
+
+                vTimer.Stop();
+                button2.Enabled = false;
+                MessageBox.Show("Таймер спрацював!", "Таймер");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            errorProvider1.Clear();
+
+
+            if (numericUpDown1.Value <= 0)
+            {
+                errorProvider1.SetError(numericUpDown1, "Кількість секунд має бути більше 0!");
+                return;
+            }
+
+
+            if (vTimer.Enabled)
+            {
+                MessageBox.Show("Таймер вже запущено!");
+                return;
+            }
+
+
+            button2.Enabled = true;
+
+
+            vTimer.Interval = 1000;
+
+
+            vTimer.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            vTimer.Stop();
+            MessageBox.Show("Таймер не встиг спрацювати!", "Таймер");
+            button2.Enabled = false;
+        }
+
+    }
+}
